f43a4e3bb4790b0011a8a81e25c7ae63bce8f418
Hossein AKhlaghpour <hossein.akhlaghpour@intapp.com>
2024-04-03 11:42:09 +0000

    https://tinyurl.com/uptakkpc Hossein Akhlaghpour

---
+++ test/unit/test_autogen.py
@@ -0,0 +1,16 @@
+import os
+import autogen
+from autogen import AssistantAgent, UserProxyAgent
+
+llm_config = {"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}
+assistant = AssistantAgent("assistant", llm_config=llm_config)
+
+user_proxy = UserProxyAgent(
+    "user_proxy", code_execution_config={"executor": autogen.coding.LocalCommandLineCodeExecutor(work_dir="coding")}
+)
+
+# Start the chat
+user_proxy.initiate_chat(
+    assistant,
+    message="Plot a chart of NVDA and TESLA stock price change YTD.",
+)
