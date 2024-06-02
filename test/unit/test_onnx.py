b712d041da2171a379bfccdd66aed2fb8dbab4ca
Hossein Akhlaghpour <hossein.akhlaghpour@intapp.com>
2024-06-02 02:27:23 +0000

    xxx

--- test/unit/test_onnx.py
+++ test/unit/test_onnx.py
@@ -3,7 +3,40 @@ from sentence_transformers import SentenceTransformer
 import onnxruntime
 
 # Load the ONNX model
-model_path = "/mnt/models/gte-large-onnx"
+model_path = "/mnt/models/gte-large-onnx/model.onnx"
+onnx_path = "/mnt/models/gte-large-onnx/model.onnx "
+
+from transformers import Pipeline
+import torch.nn.functional as F
+import torch 
+
+# copied from the model card
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+
+class SentenceEmbeddingPipeline(Pipeline):
+    def _sanitize_parameters(self, **kwargs):
+        # we don't have any hyperameters to sanitize
+        preprocess_kwargs = {}
+        return preprocess_kwargs, {}, {}
+      
+    def preprocess(self, inputs):
+        encoded_inputs = self.tokenizer(inputs, padding=True, truncation=True, return_tensors='pt')
+        return encoded_inputs
+
+    def _forward(self, model_inputs):
+        outputs = self.model(**model_inputs)
+        return {"outputs": outputs, "attention_mask": model_inputs["attention_mask"]}
+
+    def postprocess(self, model_outputs):
+        # Perform pooling
+        sentence_embeddings = mean_pooling(model_outputs["outputs"], model_outputs['attention_mask'])
+        # Normalize embeddings
+        sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
+        return sentence_embeddings
 onnx_session = onnxruntime.InferenceSession(model_path)
 
 # Define a helper function to run inference
@@ -19,7 +52,13 @@ def encode(sentences):
     return output
 
 # Example usage
-sentences = ["This is a test sentence", "Another sentence for testing"]
-embeddings = encode(sentences)
-print(embeddings)
-print(embeddings.shape)
+from optimum.onnxruntime import ORTModelForFeatureExtraction
+from transformers import AutoTokenizer
+
+model = ORTModelForFeatureExtraction.from_pretrained(onnx_path, file_name="model_optimized_quantized.onnx")
+tokenizer = AutoTokenizer.from_pretrained(onnx_path)
+
+q8_emb = SentenceEmbeddingPipeline(model=model, tokenizer=tokenizer)
+
+pred = q8_emb("Could you assist me in finding my lost card?")
+print(pred[0][:5])
