16a1aa1498eef7f1c81672ed4901a2265d8485bb
Hossein Akhlaghpour <hossein.akhlaghpour@intapp.com>
2024-06-02 11:31:32 -0700

    encoder

--- docker/encoder.py
+++ docker/encoder.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
+import json
 from typing import List
 
 app = FastAPI()
@@ -19,9 +20,18 @@ async def health() -> Response:
 @app.post("/encode")
 def encode_sentences(request: SentencesRequest):
     try:
-        embeddings = model.encode(request.sentences)      
-        return {"embeddings": embeddings}
+        ret = {"embedding": [], "token_num": 0}
+        prompts = request.sentences
+        token_counts = []
+        for prompt in prompts:
+            input_ids = model.tokenizer(prompt).input_ids
+            token_counts.append(len(input_ids))
+        embeddings = model.encode(prompts)
+        ret["embedding"] = embeddings.tolist()
+        ret["token_num"] = token_counts
+        return ret
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
