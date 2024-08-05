99caa6507948cbdc9695395de51a5514d4c1793d
Hossein Akhlaghpour <hossein.akhlaghpour@intapp.com>
2024-08-05 15:55:15 -0700

    https://tinyurl.com/dailycommits Hossein Akhlaghpour

--- docker/apps/voice/run-server.sh
+++ docker/apps/voice/run-server.sh
@@ -1,1 +1,1 @@
-gunicorn --bind 0.0.0.0:8000 --workers 1 --timeout 0 app.app:app -k uvicorn.workers.UvicornWorker
+gunicorn --bind 0.0.0.0:8000 --workers 1 --timeout 0 app:app -k uvicorn.workers.UvicornWorker
